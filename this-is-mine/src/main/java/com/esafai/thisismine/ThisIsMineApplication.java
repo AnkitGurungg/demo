69673ac32639b0c45910c69e95270c25f5c75ec7
AnkitGurung <ankitgurung234@gmail.com>
2026-06-24 03:19:54 +0545

    adding proj inside proj

---
+++ this-is-mine/src/main/java/com/esafai/thisismine/ThisIsMineApplication.java
@@ -0,0 +1,13 @@
+package com.esafai.thisismine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisIsMineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThisIsMineApplication.class, args);
+    }
+
+}
